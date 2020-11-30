00cddd58e92bd345cdabd53777a30a17604f13aa
Matt Hamlin <matthewjameshamlin@gmail.com>
2020-11-29 17:59:20 -0800

    WIP - adding ideal-idea-tool notebook entry

--- pages/notebook/index.tsx
+++ pages/notebook/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Breadcrumbs, Crumb } from '../../components/breadcrumbs'
 import { Heading, Box, Text, List, ListItem } from '@ds-pack/components'
 import LocalLink from '../../components/Link'
-// this data is collected at build time
+// this data is collected at build time - hashbuster - v1
 import notebook from /* preval */ '../../notebook'
 
 export default function Notebook() {
