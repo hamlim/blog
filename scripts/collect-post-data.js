c2ef53cb021e59b5a06a5001d31e4a83bde62afc
Matt Hamlin <matthewjameshamlin@gmail.com>
2021-02-13 16:12:56 -0800

    Add month and year extraction from frontmatter dates

--- scripts/collect-post-data.js
+++ scripts/collect-post-data.js
@@ -5,6 +5,23 @@ let path = require('path')
 let matter = require('gray-matter')
 const readingTime = require('reading-time')
 
+function getMonth(date) {
+  return {
+    0: 'January',
+    1: 'February',
+    2: 'March',
+    3: 'April',
+    4: 'May',
+    5: 'June',
+    6: 'July',
+    7: 'August',
+    8: 'September',
+    9: 'October',
+    10: 'November',
+    11: 'December',
+  }[date.getMonth()]
+}
+
 function searchRecursive(dir, pattern) {
   // This is where we store pattern matches of all files inside the directory
   let results = []
@@ -49,10 +66,19 @@ module.exports = mdxes
     const { data: frontMatter = {}, content } = matter(fileContent)
     const { text } = readingTime(content)
     frontMatter.timeToRead = text
+
+    let postDate = new Date(frontMatter.date.replace(/,|th|st|rd/g, ''))
+
+    let month = getMonth(postDate)
+
+    let year = postDate.getFullYear()
+
     return {
       absolute: `/posts/${filepath.split('pages/posts/')[1].split('.md')[0]}`,
       title: frontMatter.title || filename,
       frontMatter,
+      month,
+      year,
     }
   })
   .sort((a, b) => {
