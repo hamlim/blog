20fcfcf770c0117ea2d63f07a3f98d8819b63743
Matt Hamlin <matthewjameshamlin@gmail.com>
2022-05-21 12:57:40 -0400

    Upgrade deps

--- babel.config.js
+++ babel.config.js
@@ -1,37 +1,8 @@
-module.exports = function (api) {
-  api.cache.using(() => process.env.NODE_ENV)
-
-  let cfg = ['@babel/preset-env', { bugfixes: true }]
-  if (process.env.NODE_ENV === 'test') {
-    cfg = [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: true,
-        },
-      },
-    ]
-  }
-  return {
-    presets: [cfg, ['@babel/preset-react', { runtime: 'automatic' }]],
-    plugins: [
-      'babel-plugin-preval',
-      '@babel/plugin-transform-runtime',
-      '@babel/plugin-proposal-export-default-from',
-      [
-        '@babel/plugin-transform-typescript',
-        {
-          // force typescript to support jsx in all files no matter the extension I guess
-          isTSX: true,
-          allExtensions: true,
-          allowDeclareFields: true,
-          onlyRemoveTypeImports: true,
-        },
-      ],
-    ],
-    ignore:
-      process.env.NODE_ENV !== 'test'
-        ? ['src/test.js', 'src/__fixtures__/**/*.js']
-        : [],
-  }
+module.exports = {
+  presets: ['next/babel'],
+  plugins: [
+    'babel-plugin-preval',
+    '@babel/plugin-transform-runtime',
+    '@babel/plugin-proposal-export-default-from',
+  ],
 }
