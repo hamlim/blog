266dfd924aac32d869baad23c52dc9443872c27e
Matt Hamlin <matthewjameshamlin@gmail.com>
2023-08-18 19:17:07 -0400

    Remove old posts/content

--- old-junk/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: ['next/babel'],
-  plugins: [
-    'babel-plugin-preval',
-    '@babel/plugin-transform-runtime',
-    '@babel/plugin-proposal-export-default-from',
-  ],
-}
--- old-junk/components/record-page.tsx
+++
@@ -1,8 +0,0 @@
-export default function recordPageVisit() {
-  fetch('/api/record-page', {
-    method: 'POST',
-    body: JSON.stringify({
-      url: window.location.href,
-    }),
-  }).then((res) => res.json())
-}
