1ae6a90c682952db51fbd37a6723f508d994895f
Matt Hamlin <matthewjameshamlin@gmail.com>
2024-01-23 16:56:05 -0500

    Update resume content

--- app/(default)/resume/page.tsx
+++ app/(default)/resume/page.tsx
@@ -19,32 +19,25 @@ export default function Resume() {
           Google Docs
         </Link>, and <Link href='/resume-nov-23.pdf'>PDF</Link>.
       </Blockquote>
-      <Box className='flex flex-col md:flex-row'>
-        <Box className='md:mr-4'>
+      <Box className='flex flex-col'>
+        <Text className='mb-4'>
+          Contact me via phone:{' '}
+          <Link is='a' href='tel:4252100980'>
+            (425) 210-0980
+          </Link>{' '}
+          or email:{' '}
+          <Link is='a' href='mailto:matthewjameshamlin@gmail.com'>
+            matthewjameshamlin@gmail.com
+          </Link>
+        </Text>
+        <Box>
           <Text>
             <Text is='strong' className='font-bold'>
               Staff Software Engineer
             </Text>{' '}
-            - Strong technical leader in the frontend web development space, passionate about building the best possible
-            web user experiences and the teams that support them
+            - Strong technical leader in the frontend web development space with 10+ years of experience, passionate
+            about building the best possible web user experiences and the teams that support them
           </Text>
-        </Box>
-        <Box>
-          <address>
-            24 Hancock Street, Apt 4
-            <br /> Boston MA, 02114
-          </address>
-          <Link is='a' href='tel:4252100980'>
-            (425) 210-0980
-          </Link>
-          <br />
-          <Link is='a' href='mailto:matthewjameshamlin@gmail.com'>
-            matthewjameshamlin@gmail.com
-          </Link>
-          <br />
-          <Link href='/' is='a'>
-            https://matthamlin.me
-          </Link>
         </Box>
       </Box>
       <Stack gap={4}>
@@ -65,12 +58,13 @@ export default function Resume() {
           </Text>
           <List is='ul'>
             <ListItem>
-              Was the lead architect for a frontend-replatforming effort to adopt Next.js, React Server Components, and
-              GraphQL Federation
+              Lead architect for a frontend-replatforming effort to adopt Next.js, React Server Components, and GraphQL
+              Federation
             </ListItem>
             <ListItem>
               Designed and implemented various aspects of our overall frontend web architecture (tracking, data-fetching
-              orchestration, Authentication, web monorepo tooling)
+              orchestration, Authentication, web monorepo tooling), resulting in a more performant and better web user
+              experience for Wayfair's customers
             </ListItem>
           </List>
         </Box>
@@ -85,13 +79,13 @@ export default function Resume() {
             July 2017 - June 2021
           </Text>
           <Text>
-            Worked with a small team of frontend engineers and designers to build, and maintain our Design Systems, as
-            well as three internal component libraries for our storefront, internal, and supplier-facing websites.
+            Worked with a small team of frontend engineers and designers to build, and maintain our Design Systems, and
+            three internal component libraries for our storefront, internal, and supplier-facing websites.
           </Text>
           <List is='ul'>
             <ListItem>
-              Built out various iterations of our internal Design System component library and documentation site,
-              supporting live demos and an editable sandbox
+              Built various iterations of our internal Design System component library and documentation site, including
+              custom features like live demos and an editable sandbox
             </ListItem>
           </List>
         </Box>
@@ -106,13 +100,12 @@ export default function Resume() {
             July 2016 - July 2017
           </Text>
           <Text>
-            Worked with several teams to ship new design features and fix various bugs across the Wayfair website and
-            its affiliated brands (All Modern, Joss and Main, Birch Lane, Perigold).
+            Worked on several teams to ship new features and fix various bugs across the Wayfair website and its
+            affiliated brands (All Modern, Joss and Main, Birch Lane, Perigold).
           </Text>
           <List is='ul'>
             <ListItem>
-              I was one of the first developers to start adopting React internally, and rebuilt several key features on
-              site
+              One of the first developers to start adopting React at Wayfair, and rebuilt several key features on site
             </ListItem>
           </List>
         </Box>
@@ -134,40 +127,47 @@ export default function Resume() {
         </Box>
         <Heading is='h2'>Projects</Heading>
         <Box>
-          <Heading is='h3'>
-            Inline-MDX.macro— A Babel macro for authoring and transforming mdx
-          </Heading>
-          <Text>
-            One of my first babel macros, I built this to quickly compose MDX inline within another file for use within
-            our design system documentation site.
-          </Text>
-          <Link href='https://github.com/hamlim/inline-mdx.macro' is='a'>
-            https://github.com/hamlim/inline-mdx.macro
-          </Link>
-        </Box>
-        <Box>
-          <Heading is='h3'>
-            Simple Props— A minimal, css variable backed style-prop library for [p]react
-          </Heading>
-          <Text>
-            A CSS-in-JS library that allows you to build a system-prop-like API using plain old CSS Custom Properties
-            (variables).
-          </Text>
-          <Link href='https://github.com/ds-pack/simple-props' is='a'>
-            https://github.com/ds-pack/simple-props
-          </Link>
-        </Box>
-        <Box>
-          <Heading is='h3'>
-            Rainbow Sprinkles — Dynamic, theme-driven, style props for Vanilla Extract
-          </Heading>
-          <Text>
-            I contributed to this open source library authored by one of my friends and coworkers for building style
-            primitives within Vanilla Extract.
-          </Text>
-          <Link href='https://github.com/wayfair/rainbow-sprinkles' is='a'>
-            https://github.com/wayfair/rainbow-sprinkles
-          </Link>
+          <List is='ul'>
+            <ListItem>
+              <Text className='font-bold'>
+                Inline-MDX.macro
+              </Text>{' '}
+              <Text>A Babel macro for authoring and transforming mdx</Text>
+              <Text>
+                One of my first babel macros, I built this to quickly compose MDX inline within another file for use
+                within our design system documentation site.
+              </Text>
+              <Link href='https://github.com/hamlim/inline-mdx.macro' is='a'>
+                https://github.com/hamlim/inline-mdx.macro
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Text className='font-bold'>
+                Simple Props
+              </Text>{' '}
+              <Text>A minimal, css variable backed style-prop library for [p]react</Text>
+              <Text>
+                A CSS-in-JS library that allows you to build a system-prop-like API using plain old CSS Custom
+                Properties (variables).
+              </Text>
+              <Link href='https://github.com/ds-pack/simple-props' is='a'>
+                https://github.com/ds-pack/simple-props
+              </Link>
+            </ListItem>
+            <ListItem>
+              <Text className='font-bold'>
+                Rainbow Sprinkles
+              </Text>{' '}
+              <Text>Dynamic, theme-driven, style props for Vanilla Extract</Text>
+              <Text>
+                I contributed to this open source library authored by one of my friends and coworkers for building style
+                primitives within Vanilla Extract.
+              </Text>
+              <Link href='https://github.com/wayfair/rainbow-sprinkles' is='a'>
+                https://github.com/wayfair/rainbow-sprinkles
+              </Link>
+            </ListItem>
+          </List>
         </Box>
         <Heading is='h2'>
           Programming Languages, Libraries, and Runtimes
