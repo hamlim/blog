0e5b3566b012e049ee5db913fdc6ff27205f5dd1
Matt Hamlin <matthewjameshamlin@gmail.com>
2024-10-07 14:59:11 -0400

    more smoke tests

--- tests/smoke/homepage.spec.ts
+++ tests/smoke/homepage.spec.ts
@@ -1,8 +1,26 @@
-import { test, expect } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 test('loads the homepage', async ({ page }) => {
   await page.goto('/')
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Matt/)
+})
+
+test('loads resume', async ({ page }) => {
+  await page.goto('/resume')
+
+  await expect(page).toHaveTitle(/Resume/)
+})
+
+test('loads blog', async ({ page }) => {
+  await page.goto('/blog')
+
+  await expect(page).toHaveTitle(/Blog/)
+})
+
+test('loads projects', async ({ page }) => {
+  await page.goto('/projects')
+
+  await expect(page).toHaveTitle(/Projects/)
 })
