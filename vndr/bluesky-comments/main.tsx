e3204ac9b34f7eb600559631b57ea0ba4ad9b874
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-03-11 17:00:00 -0400

    replacing dropbox capture

--- vndr/bluesky-comments/main.tsx
+++ vndr/bluesky-comments/main.tsx
@@ -1,2 +1,2 @@
-export { CommentSection as BlueskyComments } from './CommentSection';
-export { Filters as BlueskyFilters } from './CommentFilters';
+export { CommentSection as BlueskyComments } from './CommentSection'
+export { Filters as BlueskyFilters } from './CommentFilters'
--- vndr/bluesky-comments/types.ts
+++ vndr/bluesky-comments/types.ts
@@ -1,11 +1,11 @@
 export interface CommentEmptyDetails {
-  code: string;
-  message: string;
+  code: string
+  message: string
 }
 
 export interface CommentOptions {
-  uri?: string;
-  author?: string;
-  commentFilters?: Array<(arg: any) => boolean>;
-  onEmpty?: (details: CommentEmptyDetails) => void;
+  uri?: string
+  author?: string
+  commentFilters?: Array<(arg: any) => boolean>
+  onEmpty?: (details: CommentEmptyDetails) => void
 }
